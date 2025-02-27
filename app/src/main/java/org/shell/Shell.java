85414bcfb474af1c4c39aadfc94ba0523bc074ea
StupidUsernamee <amirm.mamnoon@gmail.com>
2025-02-27 23:31:58 +0330

    IOException added.

--- app/src/main/java/org/shell/Shell.java
+++ app/src/main/java/org/shell/Shell.java
@@ -2,9 +2,11 @@ package org.shell;
 
 import org.shell.utility.ParsedData;
 
+import java.io.IOException;
+
 public class Shell {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         while (true) {
             String input = InputReader.readInput();
 
